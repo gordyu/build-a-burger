4e660fc32337290172a915ff42ee4fbf4962af23
Gordon Yu <gyunit@gmail.com>
2020-03-20 23:03:26 -0700

    removed unneeded create-react-app assets and changed font to open sans

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -183,8 +183,8 @@ module.exports = {
                         importLoaders: 1,
                         minimize: true,
                         sourceMap: shouldUseSourceMap,
-                        modules: true,
-                        localIdentName: '[name]__[local]__[has:base64:5]'
+                                          modules: true,
+                  localIdentName: '[name]__[local]__[has:base64:5]'
                       },
                     },
                     {
