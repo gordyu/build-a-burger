6f73673c80687da1cf48fd4a8d9142de4b06b3c5
Gordon Yu <gyunit@gmail.com>
2020-03-24 18:59:36 -0700

    added history using this.props.history.push({route})

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -92,21 +92,21 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState( { loading: true } );
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Max Schwarzmüller',
-                address: {
-                    street: 'Teststreet 1',
-                    zipCode: '41351',
-                    country: 'Germany'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
+        // this.setState( { loading: true } );
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Max Schwarzmüller',
+        //         address: {
+        //             street: 'Teststreet 1',
+        //             zipCode: '41351',
+        //             country: 'Germany'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
         // axios.post( '/orders.json', order )
         //     .then( response => {
         //         this.setState({ loading: false, purchasing: false });
@@ -114,6 +114,7 @@ class BurgerBuilder extends Component {
         //     .catch( error => {
         //         this.setState({ loading: false, purchasing: false });
         //     } );
+        this.props.history.push('/checkout');
     }
 
     render () {
